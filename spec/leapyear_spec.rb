d0fb935e658961843383eef504d11ad25165eb8c
root <hish.b92@gmail.com>
2020-02-22 18:43:14 +0000

    Fourth test passing

--- spec/leapyear_spec.rb
+++ spec/leapyear_spec.rb
@@ -7,11 +7,15 @@ describe LeapYear do
     end
 
     it "should return  not leapyear for years divisible by 100 but not by 400" do
-      expect(subject.year(100)).to eq 'Not Leapyear'
+      expect(subject.year(1700)).to eq 'Not Leapyear'
     end
 
     it "should return leapyear for years divisible by 4 but not by 100" do
-      expect(subject.year(4)).to eq 'Leapyear'
+      expect(subject.year(2008)).to eq 'Leapyear'
+    end
+
+    it "should return not leapyear for all years not divisible by 4" do
+      expect(subject.year(2017)).to eq 'Not Leapyear'
     end
   end
 end 
